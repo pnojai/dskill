4951d7cfb1293e706add534808f81a09ed6dcb52
Jai <jai@jeffryes.net>
2019-10-14 13:34:57 -0400

    MySQL table fix
    1. quote col name that is reserved keyword.
    2. comment out load of revised spreadsheet, not cleaned.

--- database/Proj3.sql
+++ database/Proj3.sql
@@ -8,7 +8,6 @@ USE data607proj3;
 # This is the raw data table.
 
 Drop Table if Exists rawdata;
-commit;
 Create table rawdata (
 keyword varchar(250) NOT NULL, 
 Lsrccnt int,
@@ -27,20 +26,16 @@ select * from rawdata;
 
 # This is the normalized table 
 DROP TABLE IF EXISTS dsmain;
-commit;
 CREATE TABLE dsmain(
   Year varchar(4), 
-  src varchar(40) NOT NULL,
+  `src` varchar(40) NOT NULL,
   keyword varchar(200) NOT NULL, 
-  occurs (int),
-  PRIMARY KEY (Year, src,keyword )
+  occurs int,
+  PRIMARY KEY (Year, `src`, keyword )
   );
+
+#The Powerplay number that was missing values was added with 0 to load data into table. Jan 18, 2012 to Jan 18, 2014 was changed from blank to 0.   
   
-  
-  
-  #The Powerplay number that was missing values was added with 0 to load data into table. Jan 18, 2012 to Jan 18, 2014 was changed from blank to 0. 
-  
-  
-LOAD DATA INFILE '/var/lib/mysql-files/ds_general_skills_revised.csv' 
-INTO TABLE rawdata 
-FIELDS TERMINATED BY ',';  
+# LOAD DATA INFILE '/var/lib/mysql-files/ds_general_skills_revised.csv' 
+# INTO TABLE rawdata 
+# FIELDS TERMINATED BY ',';  
