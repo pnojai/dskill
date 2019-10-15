6863a58757f97cc899db3a9c7257412cccdb058b
Jai <jai@jeffryes.net>
2019-10-15 19:49:23 -0400

    Restore MySQL load

---
+++ database/load_mysql.R
@@ -0,0 +1,27 @@
+library(RMySQL)
+
+con_local <- dbConnect(
+    drv = MySQL(),
+    host = "127.0.0.1",
+    user = "jairpt",
+    password = "a",
+    dbname = "data607proj3")
+
+con_aws <- dbConnect(
+    drv = MySQL(),
+    host = "msds607.ckxhi71v1dqf.us-east-1.rds.amazonaws.com",
+    user = "",
+    password = "",
+    dbname = "prj3")
+
+sql <- "
+select	*
+from	rawdata;"
+
+rawdata_local <- dbGetQuery(con_local, sql)
+rawdata_local
+
+dbWriteTable(conn = con_aws, name = "rawdata", value = rawdata_local, append = TRUE, row.names = FALSE)
+
+rawdata_aws <- dbGetQuery(con_aws, sql)
+rawdata_aws
